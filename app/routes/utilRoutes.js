5f9d29b386d457defdb7ad81c716776434f6caa2
Sadtan <u6000221@unimilitar.edu.co>
2020-04-11 21:15:15 -0500

    S3 frontend lazy load img

--- app/routes/utilRoutes.js
+++ app/routes/utilRoutes.js
@@ -4,32 +4,29 @@ var reqBatchHandler = require("../utils/requestBatchHandler");
 var AWSUtils = require("../utils/awsConfig.js")("archivorln");
 var awsUtils = new AWSUtils();
 
-module.exports = function (app) 
-{
-    const ResHandler      = require("../utils/responseHandler.js").ResHandler();
-    const resHandler      = new ResHandler();
+module.exports = function (app) {
+    const ResHandler = require("../utils/responseHandler.js").ResHandler();
+    const resHandler = new ResHandler();
 
     // Login Get
-    app.post("/gets3presignedurl", async (req, res) =>
-    {
-        try 
-        {
-            if (process.env.STAGE == "development")
-            {
-                var resFormat = resHandler.setResponse(200, null, await(awsUtils.getUrl(req.body.data)));
+    app.post("/gets3presignedurl", async (req, res) => {
+        try {
+            if (process.env.STAGE == "development") {
+                var resFormat = resHandler.setResponse(200, null, await (awsUtils.getUrl(req.body.data)));
                 resHandler.handleJsonResponse(res, resFormat);
-            } else if (process.env.STAGE == "testing" && req.get('origin') == "http://devremendarlonuevo.com")
-            {
-                var resFormat = resHandler.setResponse(200, null, await(awsUtils.getUrl(req.body.data)));
+            } else if (process.env.STAGE == "testing"
+                && (req.get('origin') == "http://devremendarlonuevo.com"
+                    || req.get('origin') == "devremendarlonuevo.com"
+                    || req.get('origin') == "https://devremendarlonuevo.com")
+            ) {
+                var resFormat = resHandler.setResponse(200, null, await (awsUtils.getUrl(req.body.data)));
                 resHandler.handleJsonResponse(res, resFormat);
-            } else 
-            {
+            } else {
                 var resFormat = resHandler.setResponse(400, "Define stage", {});
                 resHandler.handleJsonResponse(res, resFormat);
             }
 
-        } catch (error)
-        {
+        } catch (error) {
             console.log(error);
             resFormat = resHandler.setResponse(error.errno, error);
             resHandler.handleResponse(req, res, resFormat, "utils", "error");
