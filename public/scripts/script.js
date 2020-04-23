2034c5ca2fa63e95aac4afc78d13b1314ab54e79
Laura Junco <lc.junco10@uniandes.edu.co>
2020-04-23 15:43:13 -0500

    Agregar script de contexto sociopolitico

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -12,9 +12,30 @@ $( document ).ready(function() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 
+    //Go up
+    $('#subir').click( function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+    
     $('.slick-carrousel').slick({
         'setting-name': 'setting-value',
         'dots': 'true'
       });
 
+    //ocultar y mostrar texto de contexto sociopolitico
+    $('#mas-socio').click( function() {
+        $('.corto').addClass('hide');
+        $('.corto').removeClass('show');
+
+        $('.largo').removeClass('hide');
+        $('.largo').addClass('show');
+    });
+
+    $('#menos-socio').click( function() {
+        $('.largo').addClass('hide');
+        $('.largo').removeClass('show');
+
+        $('.corto').removeClass('hide');
+        $('.corto').addClass('show');
+    });
 });
