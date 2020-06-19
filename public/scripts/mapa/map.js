1eedf02781b6f3fab7b227372ce2093930ded343
Sadtan <u6000221@unimilitar.edu.co>
2020-06-19 10:27:39 -0500

    map style

--- public/scripts/mapa/map.js
+++ public/scripts/mapa/map.js
@@ -6,14 +6,64 @@ var docCreadores = document.getElementsByClassName("creador");
 
 var geoJson = [];
 var latLongArr = [];
-var mymap = L.map('mapid').setView([6.2486, -75.5742], 6).addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v10'));
+var mymap = L.map('mapid').setView([6.2486, -75.5742], 6).addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/light-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoic2FkdGFuIiwiYSI6ImNrYmt6ZjVpaDBmMGcydXBpeTQ0YWE1bTEifQ.KG2taHfe7j3bNm0nj2Bgug'
+}).addTo(mymap);
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 1.0,
+        fillColor: '#FC4141'
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#fff',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    mymap.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(colombiaData, {
+    style: style,
+    onEachFeature: onEachFeature
 }).addTo(mymap);
 
 for (var creador in docCreadores) {
@@ -46,7 +96,7 @@ for (var creador in docCreadores) {
         }
 
         feature.properties = {
-            'marker-color': '#126dff',
+            'marker-color': '#2727E5',
             'marker-size': 'large',
             'marker-symbol': 'circle',
             'marker-outline-color': '#000',
@@ -76,6 +126,83 @@ lugaresLayer.bindPopup(function (layer) {
     finalTXT += "</div></div>";
     return L.Util.template(finalTXT);
 })
+
+
+
+
+
+// var map = L.map('map').setView([37.8, -96], 4);
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+//     maxZoom: 18,
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+//         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     id: 'mapbox/light-v9',
+//     tileSize: 512,
+//     zoomOffset: -1
+// }).addTo(map);
+
+
+// // control that shows state info on hover
+// var info = L.control();
+
+// info.onAdd = function (map) {
+//     this._div = L.DomUtil.create('div', 'info');
+//     this.update();
+//     return this._div;
+// };
+
+// info.update = function (props) {
+//     this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+//         '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+//         : 'Hover over a state');
+// };
+
+// info.addTo(map);
+
+
+// get color depending on population density value
+// function getColor(d) {
+//     return d > 1000 ? '#800026' :
+//             d > 500  ? '#BD0026' :
+//             d > 200  ? '#E31A1C' :
+//             d > 100  ? '#FC4E2A' :
+//             d > 50   ? '#FD8D3C' :
+//             d > 20   ? '#FEB24C' :
+//             d > 10   ? '#FED976' :
+//                         '#FFEDA0';
+// }
+
+
+
+// map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+
+// var legend = L.control({position: 'bottomright'});
+
+// legend.onAdd = function (map) {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+//         labels = [],
+//         from, to;
+
+//     for (var i = 0; i < grades.length; i++) {
+//         from = grades[i];
+//         to = grades[i + 1];
+
+//         labels.push(
+//             '<i style="background:' + getColor(from + 1) + '"></i> ' +
+//             from + (to ? '&ndash;' + to : '+'));
+//     }
+
+//     div.innerHTML = labels.join('<br>');
+//     return div;
+// };
+
+// legend.addTo(map);
+
 
 
 
