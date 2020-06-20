bcaad6fe8e6d785fc28a286b1f8154bcb0ad7fc5
Laura Junco <lc.junco10@uniandes.edu.co>
2020-06-20 16:36:08 -0500

    Arreglos mapa

--- public/scripts/mapa/map.js
+++ public/scripts/mapa/map.js
@@ -6,7 +6,7 @@ var docCreadores = document.getElementsByClassName("creador");
 
 var geoJson = [];
 var latLongArr = [];
-var mymap = L.map('mapid', { zoomControl: false }).setView([4.2486, -60.5742], 6);
+var mymap = L.map('mapid', { zoomControl: false }).setView([6.2486, -75.5742], 6);
 //var mymap = L.map('mapid').setView([6.2486, -75.5742], 6).addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
