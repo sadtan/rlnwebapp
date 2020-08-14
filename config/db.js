af38a1269982f8f6297df579fadbbdf9c0c127a5
Sadtan <u6000221@unimilitar.edu.co>
2020-08-14 11:36:05 -0500

    prod migration

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 	connection: {
-		host: "database-1.ccd10d51wnps.us-east-2.rds.amazonaws.com",
+		host: "rln-prod.ccd10d51wnps.us-east-2.rds.amazonaws.com",
 		user: process.env.SQL_USER,
 		password: process.env.SQL_PASS
 	},
--- dbpool.js
+++ dbpool.js
@@ -2,7 +2,7 @@
 const MysqlCache = require('mysql-cache')
 
 var parsedCredentials = {
-    host: "database-1.ccd10d51wnps.us-east-2.rds.amazonaws.com",
+    host: "rln-prod.ccd10d51wnps.us-east-2.rds.amazonaws.com",
     user: process.env.SQL_USER,
     password: process.env.SQL_PASS,
     database: "undbtest",
